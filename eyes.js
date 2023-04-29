3719a0ae87d1ca6e503f7a5b0589dbd36b50debf
Angel Rangel <105447969+arang16180@users.noreply.github.com>
2023-04-29 13:51:15 -0600

    Add files via upload

---
+++ eyes.js
@@ -0,0 +1,42 @@
+
+
+const eyesNode = document.getElementById('eyes');
+
+for (let i=0;i<=1;i++){
+  var eyeArray = [];
+  eyeArray[i] = document.createElement("div");
+  eyeArray[i].classList.add('eye');
+  eyesNode.appendChild(eyeArray[i]);
+  
+  var ballArray = []
+  ballArray[i] = document.createElement("div");
+  ballArray[i].classList.add('ball');
+  eyeArray[i].appendChild(ballArray[i]);
+  //console.log(ojos);
+
+  document.getElementById('eyes').style.verticalAlign = "middle";
+  document.getElementById('eyes').style.left = "25%";
+  document.getElementById('eyes').style.position = "absolute";
+  document.getElementById('eyes').style.top = "35%";
+}
+
+const balls = document.getElementsByClassName('ball');
+
+  //const neweye = document.createElement("div");
+  //neweye.classList.add('eye','ball');
+  //balls.
+
+
+document.onmousemove = (event) => {
+  const x = (event.clientX * 100) / window.innerWidth + '%';
+  const y = (event.clientY * 100) / window.innerHeight + '%';
+
+  for(let i = 0;i<=1;i++){
+  balls[i].style.left = x;
+  balls[i].style.top = y;
+  balls[i].transform = 'translate(-' + x + ',-' + y + ')';
+
+  }
+
+
+};
